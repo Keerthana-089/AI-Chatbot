332e849c0ee28da09dce1f8a402c91ecd54181fb
KLU <2420030581@klh.edu.in>
2026-04-03 12:08:46 +0530

    added Gemini AI

--- api/main.py
+++ api/main.py
@@ -1,36 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import os
 import google.generativeai as genai
+import os
 
 app = FastAPI()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-print("API KEY:", os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 
 class ChatRequest(BaseModel):
     message: str
 
-
-@app.get("/")
-def home():
-    return {"message": "Xylo AI is ready 🚀"}
-
-
 @app.post("/chat")
 def chat(req: ChatRequest):
     try:
-        print("User:", req.message)
-
         response = model.generate_content(req.message)
-
-        print("Gemini:", response.text)
-
         return {"response": response.text}
-
     except Exception as e:
-        print("ERROR:", str(e))
         return {"response": "⚠️ Error: " + str(e)}
